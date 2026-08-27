54240477cc9f5a83b78b02df3018b044e9106ee0
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-27 17:53:08 +0300

    create task_list entity and then add id column

---
+++ src/main/java/com/example/demo/domain/entities/TaskList.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cache;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "task_list")
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id",updatable = false, nullable = false)
+    private UUID id;
+}
