2e8a0cad26a22456fc95134c7a5a6bdeac65331c
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-28 14:47:26 +0300

    add fromDto method in task mapper

--- src/main/java/com/example/demo/mappers/TaskMapper.java
+++ src/main/java/com/example/demo/mappers/TaskMapper.java
@@ -1,4 +1,10 @@
 package com.example.demo.mappers;
 
+import com.example.demo.domain.dto.TaskDto;
+import com.example.demo.domain.entities.Task;
+
 public interface TaskMapper {
+
+   Task fromDto(TaskDto taskDto);
+
 }
