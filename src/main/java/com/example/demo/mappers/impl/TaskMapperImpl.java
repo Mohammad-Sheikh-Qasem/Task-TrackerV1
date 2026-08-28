438eb014698e5b6870bfb4707c75d1e8a90d733a
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-28 14:51:37 +0300

    add new packge inside mappers named by impl then create TaskMapperImpl class and implements to TaskMapper interface

---
+++ src/main/java/com/example/demo/mappers/impl/TaskMapperImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.mappers.impl;
+
+import com.example.demo.domain.dto.TaskDto;
+import com.example.demo.domain.entities.Task;
+import com.example.demo.mappers.TaskMapper;
+
+public class TaskMapperImpl implements TaskMapper {
+    @Override
+    public Task fromDto(TaskDto taskDto) {
+        return null;
+    }
+
+    @Override
+    public TaskDto toDto(Task task) {
+        return null;
+    }
+}
