f6395ebf2e96590ab52c9b797065f840c2300863
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-28 15:15:56 +0300

    declare the TaskMapperImpl class as @Component and and then implement the toDto method

--- src/main/java/com/example/demo/mappers/impl/TaskMapperImpl.java
+++ src/main/java/com/example/demo/mappers/impl/TaskMapperImpl.java
@@ -3,8 +3,12 @@ package com.example.demo.mappers.impl;
 import com.example.demo.domain.dto.TaskDto;
 import com.example.demo.domain.entities.Task;
 import com.example.demo.mappers.TaskMapper;
+import jakarta.persistence.Column;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TaskMapperImpl implements TaskMapper {
+
     @Override
     public Task fromDto(TaskDto taskDto) {
         return new Task(
@@ -22,6 +26,14 @@ public class TaskMapperImpl implements TaskMapper {
 
     @Override
     public TaskDto toDto(Task task) {
-        return null;
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueData(),
+                task.getStatus(),
+                task.getPriority()
+
+        );
     }
 }
