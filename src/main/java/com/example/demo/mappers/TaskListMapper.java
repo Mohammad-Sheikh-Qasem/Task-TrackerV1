117c5627eab5ebfcd0587095c5d2f4a4a276aa04
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-29 13:50:24 +0300

    add fromDto method in TaskListMapper interface

--- src/main/java/com/example/demo/mappers/TaskListMapper.java
+++ src/main/java/com/example/demo/mappers/TaskListMapper.java
@@ -1,4 +1,9 @@
 package com.example.demo.mappers;
 
+import com.example.demo.domain.dto.TaskListDto;
+import com.example.demo.domain.entities.TaskList;
+
 public interface TaskListMapper {
+
+    TaskList fromDto(TaskListDto taskListDto);
 }
