790d6b351812935753c193268a13425173ab3f26
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-29 14:26:45 +0300

    extend TaskRepository to JpaRepository with type of entity Task and type of task UUID

--- src/main/java/com/example/demo/repositories/TaskRepository.java
+++ src/main/java/com/example/demo/repositories/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repositories;
 
-public interface TaskRepository {
+import com.example.demo.domain.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
