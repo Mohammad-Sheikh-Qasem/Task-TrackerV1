e0770c5356844875c8f97111d555e61491557296
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-29 14:28:58 +0300

    add findByTaskListId method -> interface TaskRepository

--- src/main/java/com/example/demo/repositories/TaskRepository.java
+++ src/main/java/com/example/demo/repositories/TaskRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repositories;
 import com.example.demo.domain.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByTaskListId(UUID taskListId);
 }
