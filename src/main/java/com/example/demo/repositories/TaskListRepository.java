79f137cdd4283ba07bc59c11b7977b554936e94c
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-29 14:22:54 +0300

    extend TaskListRepository to JpaRepository with type of entity TaskList and the type of TaskList UUID

--- src/main/java/com/example/demo/repositories/TaskListRepository.java
+++ src/main/java/com/example/demo/repositories/TaskListRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repositories;
 
+import com.example.demo.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface TaskListRepository {
+public interface TaskListRepository extends JpaRepository<TaskList, UUID>{
 }
