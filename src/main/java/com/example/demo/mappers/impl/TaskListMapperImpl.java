6e2eedfcb4c10ea667753df3e048440d64f356cc
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-29 14:02:23 +0300

    fromDto implemented

--- src/main/java/com/example/demo/mappers/impl/TaskListMapperImpl.java
+++ src/main/java/com/example/demo/mappers/impl/TaskListMapperImpl.java
@@ -3,13 +3,33 @@ package com.example.demo.mappers.impl;
 import com.example.demo.domain.dto.TaskListDto;
 import com.example.demo.domain.entities.TaskList;
 import com.example.demo.mappers.TaskListMapper;
+import com.example.demo.mappers.TaskMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class TaskListMapperImpl implements TaskListMapper {
+
+    private final TaskMapper taskMapper;
+
+    public TaskListMapperImpl(TaskMapper taskMapper) {
+        this.taskMapper = taskMapper;
+    }
+
+
     @Override
     public TaskList fromDto(TaskListDto taskListDto) {
         return new TaskList(
+                taskListDto.id(),
+                taskListDto.title(),
+                taskListDto.description(),
+                Optional.ofNullable(taskListDto.tasks())
+                        .map(tasks -> tasks.stream().map(taskMapper::fromDto)
+                                .toList()
+                        ).orElse(null),
+                null,
+                null
 
         );
     }
