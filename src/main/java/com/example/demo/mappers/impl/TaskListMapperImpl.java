fadcf80d84adf41ce2700b8b1351f95b55486a63
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-29 13:54:31 +0300

    implement TaskListMapperImpl to TaskListMapper

--- src/main/java/com/example/demo/mappers/impl/TaskListMapperImpl.java
+++ src/main/java/com/example/demo/mappers/impl/TaskListMapperImpl.java
@@ -1,4 +1,17 @@
 package com.example.demo.mappers.impl;
 
-public class TaskListMapperImpl {
+import com.example.demo.domain.dto.TaskListDto;
+import com.example.demo.domain.entities.TaskList;
+import com.example.demo.mappers.TaskListMapper;
+
+public class TaskListMapperImpl implements TaskListMapper {
+    @Override
+    public TaskList fromDto(TaskListDto taskListDto) {
+        return null;
+    }
+
+    @Override
+    public TaskListDto toDto(TaskList taskList) {
+        return null;
+    }
 }
