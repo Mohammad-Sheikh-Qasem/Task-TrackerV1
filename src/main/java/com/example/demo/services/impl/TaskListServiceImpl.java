f5ce37a8e75cef36eb2231567614fb8714d5b32a
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-30 14:38:09 +0300

    declare TaskListServiceImpl class as a @Service

--- src/main/java/com/example/demo/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/TaskListServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.demo.services.impl;
 
 import com.example.demo.domain.entities.TaskList;
 import com.example.demo.services.TaskListService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskListServiceImpl implements TaskListService {
 
     @Override
