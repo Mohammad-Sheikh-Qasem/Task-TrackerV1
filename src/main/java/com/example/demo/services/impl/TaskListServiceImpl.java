478ba9d00bb5d377843c8a682f5d64db5cc8e48e
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-30 14:37:22 +0300

    implement TaskListServiceImpl to TaskListService interface

--- src/main/java/com/example/demo/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/TaskListServiceImpl.java
@@ -1,4 +1,14 @@
 package com.example.demo.services.impl;
 
-public class TaskListServiceImpl {
+import com.example.demo.domain.entities.TaskList;
+import com.example.demo.services.TaskListService;
+
+import java.util.List;
+
+public class TaskListServiceImpl implements TaskListService {
+
+    @Override
+    public List<TaskList> listTaskLists() {
+        return List.of();
+    }
 }
