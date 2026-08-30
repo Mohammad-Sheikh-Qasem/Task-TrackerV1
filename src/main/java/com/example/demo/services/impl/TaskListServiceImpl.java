72079be5fc9b897ab92564f69b35e01249873734
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-30 14:39:45 +0300

    listTaskLists implemented

--- src/main/java/com/example/demo/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/TaskListServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.services.impl;
 
 import com.example.demo.domain.entities.TaskList;
+import com.example.demo.repositories.TaskListRepository;
 import com.example.demo.services.TaskListService;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,14 @@ import java.util.List;
 @Service
 public class TaskListServiceImpl implements TaskListService {
 
+    private final TaskListRepository taskListRepository;
+
+    public TaskListServiceImpl(TaskListRepository taskListRepository) {
+        this.taskListRepository = taskListRepository;
+    }
+
     @Override
     public List<TaskList> listTaskLists() {
-        return List.of();
+        return taskListRepository.findAll();
     }
 }
