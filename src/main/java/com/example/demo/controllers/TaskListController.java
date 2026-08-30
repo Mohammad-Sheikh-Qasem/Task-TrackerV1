b4cfb3597d38d80b487b4fe24da6624900535e91
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-30 14:46:45 +0300

    implement task list controller listing endpoint

--- src/main/java/com/example/demo/controllers/TaskListController.java
+++ src/main/java/com/example/demo/controllers/TaskListController.java
@@ -1,9 +1,34 @@
 package com.example.demo.controllers;
 
+import com.example.demo.domain.dto.TaskListDto;
+import com.example.demo.mappers.TaskListMapper;
+import com.example.demo.services.TaskListService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/task-lists")
 public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+
+    @GetMapping
+    public List<TaskListDto> listDtoList(){
+
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+
+    }
 }
