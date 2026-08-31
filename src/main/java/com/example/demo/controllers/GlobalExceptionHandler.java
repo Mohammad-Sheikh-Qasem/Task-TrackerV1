f6dab1af1428bd3d06fdd53fef5c2d6b26c712c2
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-31 17:13:19 +0300

    add handleExceptions method with RuntimeException,WebRequest signture params

--- src/main/java/com/example/demo/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controllers/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    public ResponseEntity<ErrorResponse> handleExceptions(
+            RuntimeException ex, WebRequest request
+    ){
+
+    }
 }
